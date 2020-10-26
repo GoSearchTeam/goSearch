4c8d882728a829e81bd076162f0f45c13c2da799
BenSegal855 <bensegal@udel.edu>
2020-10-26 18:45:30 -0400

    crapy search test

--- indexing_test.go
+++ indexing_test.go
@@ -19,5 +19,38 @@ func TestAddIndex(t *testing.T) {
 	if _, err := os.Stat(doc); os.IsNotExist(err) {
 		t.Errorf("%s not created", doc)
 	}
-	os.Remove(doc)
+	ids := []uint64{id}
+	documentsCleanup(ids)
+}
+
+func TestSearch(t *testing.T) {
+	app := initApp("TestAddIndex")
+	fields := []string{"name"}
+	tacoData, _ := parseArbJSON(`{
+		"name": "tacos"
+	}`)
+	tacoID := app.addIndex(tacoData)
+
+	burgerData, _ := parseArbJSON(`{
+		"name": "burger"
+	}`)
+	burgerID := app.addIndex(burgerData)
+
+	tacoSearch, _ := app.search("tacos", fields, false)
+	burgerSearch, _ := app.search("burger", fields, false)
+
+	ids := []uint64{tacoID, burgerID}
+	documentsCleanup(ids)
+
+	if tacoSearch[0] != tacoID || burgerSearch[0] != burgerID {
+		t.Error("ID's didnt match")
+	}
+
+}
+
+func documentsCleanup(docs []uint64) {
+	for _, id := range docs {
+		doc := fmt.Sprintf("./documents/%d", id)
+		os.Remove(doc)
+	}
 }
