7b26e6e87ddc94a1bab651c4b7ec549689cc46ab
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2020-10-28 16:44:32 -0400

    Updated Health Check endpoint (basic am i alive)

--- testEPs.go
+++ testEPs.go
@@ -7,16 +7,7 @@ import (
 )
 
 func HandleTestRoutes(r *gin.Engine) {
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"yer": "ye",
-		})
-	})
-
-	r.POST("/test", func(c *gin.Context) {
-		data, _ := ioutil.ReadAll(c.Request.Body)
-		jDat, _ := parseArbJSON(string(data))
-		log.Println(jDat["test"])
-		c.String(200, "ok")
+	r.GET("/hc", func(c *gin.Context) {
+		c.Status(200)
 	})
 }
