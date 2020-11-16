91c7ce62804c738fa8db42a4e2e0a469625fac82
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2020-11-16 13:27:09 -0500

    search

--- performanceTesting/index.js
+++ performanceTesting/index.js
@@ -2,6 +2,7 @@ const faker = require('faker')
 const nanoid = require('nanoid/non-secure').nanoid
 const fetch = require('isomorphic-fetch')
 const fs = require('fs')
+const nread = require('n-readlines')
 
 const uploadItem = async () => {
   const userObj = {
@@ -37,13 +38,54 @@ const uploadTest = async (rounds) => {
     const item = await uploadItem()
     // Store every 100 items
     if (i % 100 === 0) {
+      console.log(i)
       fs.appendFile('./randomItems.txt', `${JSON.stringify(item)}\n`, () => {})
     }
   }
 }
 
+const searchItem = async (itemField) => {
+  const start = process.hrtime.bigint()
+  const resp = await fetch(`http://${process.env.HOSTNAME}:8080/index/search`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      query: itemField
+    })
+  })
+  if (resp.status > 299) {
+    console.log(await resp.text())
+  }
+  const end = process.hrtime.bigint()
+  const diffTime = end - start
+  fs.appendFile('./searchItems.csv', `${Number(diffTime) / 1000000}\n`, () => {}) // milliseconds
+}
+
+const randomProperty = function (obj) {
+  const keys = Object.keys(obj)
+  return obj[keys[keys.length * Math.random() << 0]]
+}
+
+const searchTest = async () => {
+  const items = []
+  const liner = new nread('./randomItems.txt')
+  let line
+  while (line = liner.next()) {
+    items.push(JSON.parse(line))
+  }
+  for (let i = 0; i < 1000; i++) {
+    // get random item field
+    console.log(items[i])
+    const theKey = randomProperty(items[i])
+    await searchItem(items[i][theKey])
+  }
+}
+
 const main = async () => {
-  uploadTest(10)
+  // await uploadTest(100000)
+  await searchTest()
 }
 
 main()
