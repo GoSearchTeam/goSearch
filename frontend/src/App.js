e91f61b1a808b2179ad956b301315ec69eac20f8
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2020-11-17 17:14:36 -0500

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
-import LoginPage from "./LoginPage.js";
-import Dashboard from "./Dashboard.js";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.js'
+import Dashboard from './pages/Dashboard.js'
 
-function App() {
+function App () {
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
+      <div className='App'>
+        <header className='App-header'>
           Go Search (Admin Land)
         </header>
         <Switch>
-          <Route path="/admin/login">
-            <LoginPage></LoginPage>
-          </Route>
-          <Route path="/admin/dashboard">
-            <Dashboard></Dashboard>
-          </Route>
+          {/* THE ORDER HERE MATTERS, which ever matches first will load i.e. /admin will load /admin/dashboard if put first */}
+          <Route path='/admin/dashboard' render={props => <Dashboard {...props} />} />
+          <Route path='/admin' render={props => <LoginPage {...props} />} />
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
