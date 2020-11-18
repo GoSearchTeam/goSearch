2a49a2cd65676ff4651b6b91abb936a57434fc0f
BenSegal855 <bensegal@udel.edu>
2020-11-17 23:14:17 -0500

    Basic stats page with dummy functions

--- frontend/src/pages/Stats.js
+++ frontend/src/pages/Stats.js
@@ -1,7 +1,25 @@
+import { Badge } from 'react-bootstrap'
+
 export default function Stats(props) {
     return (
         <>
-            <header>Tacos</header>
-        </>   
+            <header className="Section-header">Genaral Status</header>
+            <br></br>
+            Nodes Running <Badge variant="dark">{getNodes()}</Badge><br></br>
+            Documents on Disk <Badge variant="dark">{getDocs()}</Badge><br></br>
+            Indexes Currently Searchable <Badge variant="dark">{getIndexes()}</Badge><br></br>
+        </>
     );
+}
+
+function getNodes() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+function getDocs() {
+    return Math.floor(Math.random() * 1000000) + 100;
+}
+
+function getIndexes() {
+    return Math.floor(Math.random() * 30) + 1;
 }
