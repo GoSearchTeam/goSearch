c927884c80d274a6d74fd844cf8e9cdada0b72b9
Ben <ben@segals.org>
2021-04-15 13:49:27 -0400

    Management page half functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Nav } from 'react-bootstrap'
 import LoginPage from './pages/LoginPage.js'
-import Managment from './pages/Managment.js'
+import Management from './pages/Managment.js'
 import { Stats } from './pages/Stats.js'
 
 function App() {
@@ -11,16 +11,16 @@ function App() {
     <Router>
       <div className='App'>
         <Navbar bg="dark" variant="dark" expand="lg" className="nav-bar-round">
-          <Navbar.Brand href="./managment">Go Search</Navbar.Brand>
+          <Navbar.Brand href="./management">Go Search</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="./managment">Management</Nav.Link>
+            <Nav.Link href="./management">Management</Nav.Link>
             <Nav.Link href="./stats">Status</Nav.Link>
           </Nav>
         </Navbar>
         <br></br>
         <Switch>
           {/* THE ORDER HERE MATTERS, which ever matches first will load i.e. /admin will load /admin/dashboard if put first */}
-          <Route path='/admin/managment' render={props => <Managment {...props} />} />
+          <Route path='/admin/management' render={props => <Management {...props} />} />
           <Route path='/admin/stats' render={props => <Stats {...props} />} />
           <Route path='/admin' render={props => <LoginPage {...props} />} />
         </Switch>
--- frontend/src/pages/Managment.js
+++ frontend/src/pages/Managment.js
@@ -1,55 +1,182 @@
-// import './App.css';
-import { Form, Button, Col } from 'react-bootstrap'
+import { useState } from 'react'
+import { Form, Button, Col, Alert } from 'react-bootstrap'
+import { ENDPOINTS } from '../config'
 
-export default function Managment (props) {
-  return (
-    <>
-      <Form>
-        <Form.Group controlId='dashboard.Search'>
-          <h3 className='Section-header'>Search Documents</h3>
-          <Form.Row>
-            <Col>
-              <Form.Control placeholder='Fields' className='bg-dark' />
-            </Col>
-            <Col>
-              <Form.Control placeholder='Value' className='bg-dark' />
-            </Col>
-            <Col>
-              <Button variant='secondary' className='bg-dark' type='submit'>
-                                Search
-              </Button>
-            </Col>
-          </Form.Row>
-        </Form.Group>
-        <Form.Group controlId='dashboard.AddIndex'>
-          <h3 className='Section-header'>Add Documents</h3>
-          <Form.Control as='textarea' className='bg-dark' rows={7} />
+export default function Management(props) {
+  const [searchError, setSearchError] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
+  const [searchResult, setSearchResult] = useState('Loading...')
+
+  const [showAddError, setShowAddError] = useState(false)
+  const [addError, setAddError] = useState('')
+  const [showAddResult, setShowAddResult] = useState(false)
+  const [addResult, setAddResult] = useState('Loading...')
+
+  return <div>
+    {/* Search Form */}
+    <Form onSubmit={(e) => {
+      e.preventDefault()
+      const formData = acquireData(e)
+      setShowSearch(false)
+      setSearchError(false)
+      setSearchResult('Loading...')
+
+      fetch(ENDPOINTS.SEARCH, {
+        method: 'POST',
+        body: JSON.stringify({
+          query: formData.value,
+          fields: [formData.index]
+        })
+      }).then(res => res.json())
+      .then(data => {
+        setShowSearch(true)
+        setSearchResult(JSON.stringify(data, null, 2))
+      })
+      .catch(() => {
+        setSearchError(true)
+        setShowSearch(false)
+        setSearchResult('Loading...')
+      })
+    }}>
+      <h3 className='Section-header'>Search Documents</h3>
+      <Form.Row>
+        <Col>
+          <Form.Control name='index' type='text' placeholder='Index' className='bg-dark' id='search-index' />
+        </Col>
+        <Col>
+          <Form.Control name='value' type='text' placeholder='Value' className='bg-dark' id='search-value' />
+        </Col>
+        <Col>
           <Button variant='secondary' className='bg-dark' type='submit'>
-                        Add Document
+            Search
           </Button>
+        </Col>
+      </Form.Row>
+      {searchError &&
+        <Alert className='search-error' variant='danger' onClose={() => setSearchError(false)} dismissible>
+          An Error occurred
+        </Alert>
+      }
+      {showSearch &&
+        <Alert
+          className='search-error'
+          variant='dark'
+          onClose={() => {
+            setShowSearch(false)
+            setSearchResult('Loading...')
+          }}
+          dismissible
+        >
+          <pre>{searchResult}</pre>
+        </Alert>
+      }
+    </Form>
+    <br/>
+
+    {/* Add Documents Form */}
+    <Form onSubmit={(e) => {
+      e.preventDefault()
+
+      setAddError('')
+      setShowAddError(false)
+      setAddResult('Loading...')
+      setShowAddResult(false)
+
+      const { document } = acquireData(e)
+
+      try {
+        JSON.parse(document)
+      } catch {
+        setShowAddError(true)
+        setAddError('Please enter Valid JSON.')
+        return
+      }
+
+      fetch(ENDPOINTS.ADD, {
+        method: 'POST',
+        body: document
+      }).then(res => res.json())
+      .then(data => {
+        setShowAddResult(true)
+        setAddResult(`${data.msg}: ${data.docID}`)
+      }).catch(() => {
+        setShowAddError(true)
+        setAddError('An error occurred when attempting to add the document')
+      })
+    }}>
+      <h3 className='Section-header'>Add Documents</h3>
+      <Form.Control name='document' as='textarea' placeholder='Valid JSON' className='bg-dark' rows={7} />
+      <Button variant='secondary' className='bg-dark' type='submit'>
+        Add Document
+      </Button>
+      {showAddError &&
+        <Alert className='add-error' variant='danger' onClose={() => setShowAddError(false)} dismissible>
+          {addError}
+        </Alert>
+      }
+      {showAddResult &&
+        <Alert
+          className='add-result'
+          variant='dark'
+          onClose={() => {
+            setShowAddResult(false)
+            setAddResult('Loading...')
+          }}
+          dismissible
+        >
+          {addResult}
+        </Alert>
+
+      }
+    </Form>
+    <br/>
+
+    {/* Update Doucments form */}
+    <Form onSubmit={(e) => {
+      e.preventDefault()
+      const formData = acquireData(e)
+      console.log(formData)
+    }}>
+      <h3 className='Section-header'>Update Document</h3>
+      <Form.Control name='docId' type='text' placeholder='Document ID' className='bg-dark' />
+      <Form.Control name='docValue' as='textarea' placeholder='Valid JSON' className='bg-dark' rows={6} />
+      <Button variant='secondary' className='bg-dark' type='submit'>
+        Update Document
+      </Button>
+    </Form>
+    <br/>
+
+    {/* Delete documents form */}
+    <Form onSubmit={(e) => {
+      e.preventDefault()
+      const formData = acquireData(e)
+      console.log(formData)
+    }}>
+      <h3 className='Section-header'>Delete Document</h3>
+      <Form.Row>
+        <Col>
+          <Form.Control name='docId' type='text' placeholder='Document ID' className='bg-dark' />
+        </Col>
+        <Col>
+          <Button variant='danger' type='submit'>
+            Delete
+          </Button>
+        </Col>
+      </Form.Row>
+    </Form>
+    {/* <Form.Group controlId='dashboard.AddIndex'>
+          
         </Form.Group>
         <Form.Group controlId='dashboard.UpdateIndex'>
-          <h3 className='Section-header'>Update Document</h3>
-          <Form.Control placeholder='Document ID' className='bg-dark' />
-          <Form.Control as='textarea' className='bg-dark' rows={6} />
-          <Button variant='secondary' className='bg-dark' type='submit'>
-                        Update Document
-          </Button>
+          
         </Form.Group>
         <Form.Group controlId='dashboard.Delete'>
-          <h3 className='Section-header'>Delete Document</h3>
-          <Form.Row>
-            <Col>
-              <Form.Control placeholder='Document ID' className='bg-dark' />
-            </Col>
-            <Col>
-              <Button variant='danger' type='submit'>
-                                Delete
-              </Button>
-            </Col>
-          </Form.Row>
+          
         </Form.Group>
-      </Form>
-    </>
-  )
+      </Form> */}
+  </div>
+}
+
+function acquireData(event) {
+  return Object.fromEntries(new FormData(event.target).entries())
 }
